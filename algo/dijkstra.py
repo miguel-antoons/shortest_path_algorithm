3deda87ec3f56ae72e67b8b55c1cb3a77e3701bd
miguel-antoons <miguel.antoons@gmail.com>
2022-12-16 13:24:32 +0100

    adding dijkstra algorithm

--- algo/dijkstra.py
+++ algo/dijkstra.py
@@ -4,7 +4,7 @@ import numpy as np
 def find_min(row, fridge):
     min = np.inf
     index = None
-    for col in range(row):
+    for col in range(row.size):
         if row[col] < min and col not in fridge:
             min = row[col]
             index = col
@@ -13,25 +13,26 @@ def find_min(row, fridge):
 
 
 def Dijkstra(C: np.matrix) -> np.matrix:
+    size = C.shape[0]
     D = np.matrix(np.ones(C.shape) * np.inf)
 
-    for row in range(C):
+    for row in range(size):
         fridge = []
-        D[row] = [np.inf for _ in range(D[row])]
         D[row, row] = 0
 
-        for i in range(C):
-            min = find_min(D[row], fridge)
+        for _ in range(size):
+            min = find_min(np.array(D[row])[0], fridge)
             fridge.append(min)
 
-            for j in range(C):
+            for j in range(size):
                 # if the path from the minimal node is a neighbor of the
                 # current node and the current node is not in the fridge
-                if C[min, j] > 0 and j not in fridge:
+                if j not in fridge:
                     # update the minimal distance with the the minimal node
                     # and the distance from the minimal node to the current
                     # path if that distance is smaller than the current
                     # minimal distance
-                    D[row, j] = min(D[row, j], D[row, min] + C[min, j])
+                    D[row, j] = D[row, min] + C[min, j] if D[row, j] > D[row, min] + C[min, j] else D[row, j]
+                    # D[row, j] = min(D[row, j], D[row, min] + C[min, j])
 
     return D
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import numpy as np
 from algo.bellmanFord import bellman_ford
+from algo.dijkstra import Dijkstra
 
 
 def main():
-    test   = open_csv("csv/graph44.csv")
-    result = bellman_ford(test)
+    test   = open_csv("csv/graph_clem.csv")
+    # result = bellman_ford(test)
+    result = Dijkstra(test)
     print(result)
 
 # function to open csv and return matrix of int
 def open_csv(file):
-    matrix = np.genfromtxt(file, delimiter=',', dtype=int)
+    matrix = np.genfromtxt(file, delimiter=',', dtype=float)
     return matrix
 
 if __name__ == '__main__':
