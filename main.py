53c998d28e8eae33aae555bc59bcfb8c738bb193
nmartou <martounicolas@gmail.com>
2022-12-16 13:32:29 +0100

    merge conflict

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 from algo.bellmanFord import bellman_ford
-from algo.dijkstra import Dijkstra
-
+from algo.Floyd_Warshall import Floyd_Warshall
 
 def main():
-    test   = open_csv("csv/graph_clem.csv")
+    test   = open_csv("csv/graph44.csv")
     # result = bellman_ford(test)
-    result = Dijkstra(test)
+    # print(result)
+    result = Floyd_Warshall(test)
     print(result)
 
 # function to open csv and return matrix of int
